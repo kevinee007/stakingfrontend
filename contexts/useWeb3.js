5bc4d5eb92066f569944dd71327689511eee626b
Samuel Safahi <60489622+samisbakedham@users.noreply.github.com>
2022-01-24 11:28:45 +0000

    remov

--- contexts/useWeb3.js
+++ contexts/useWeb3.js
@@ -134,36 +134,4 @@ export function useWeb3() {
 }
 
 
-
-
-
-const tokenAddress = '0x5423063af146F5abF88Eb490486E6B53FA135eC9';
-const tokenSymbol = 'CNDL';
-const tokenDecimals = 18;
-const tokenImage = 'https://raw.githubusercontent.com/CandlePlatforms/Docs/main/logo.svg';
-
-try {
-  // wasAdded is a boolean. Like any RPC method, an error may be thrown.
-  const wasAdded = await ethereum.request({
-    method: 'wallet_watchAsset',
-    params: {
-      type: 'ERC20', // Initially only supports ERC20, but eventually more!
-      options: {
-        address: tokenAddress, // The address that the token is at.
-        symbol: tokenSymbol, // A ticker symbol or shorthand, up to 5 chars.
-        decimals: tokenDecimals, // The number of decimals in the token
-        image: tokenImage, // A string url of the token logo
-      },
-    },
-  });
-
-  if (wasAdded) {
-    console.log('Thanks for your interest!');
-  } else {
-    console.log('Your loss!');
-  }
-} catch (error) {
-  console.log(error);
-}
-
 export default useWeb3
